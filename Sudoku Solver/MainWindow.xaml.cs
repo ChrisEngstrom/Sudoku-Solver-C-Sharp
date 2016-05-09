dcb09962c915da95df09be1e4b0f0dead4abac86
Chris Engstrom <tipher88@gmail.com>
2016-05-09 00:47:10 -0500

    Started laying out the Sudoku board
    Programmatically added all the text boxes to the main GameBoard grid
    with the proper spacing. That should be more efficient instead of having
    3 grid rows and 3 grid columns, then adding 9 text boxes to each.

--- Sudoku Solver/MainWindow.xaml.cs
+++ Sudoku Solver/MainWindow.xaml.cs
@@ -18,8 +18,64 @@ namespace Sudoku_Solver {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        const int SQUARE_SIZE = 50;
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Window_Loaded( object sender, RoutedEventArgs e ) {
+            GenerateControls();
+        }
+
+        private void GenerateControls() {
+            for ( int row = 0; row < 9; row++ ) {
+                for ( int col = 0; col < 9; col++ ) {
+                    AddTextBox( row, col );
+                }
+            }
+        }
+
+        private void AddTextBox( int row, int col ) {
+            TextBox textBox = new TextBox();
+            textBox.VerticalContentAlignment = System.Windows.VerticalAlignment.Center;
+            textBox.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Center;
+            textBox.FontSize = 28;
+            textBox.FontWeight = FontWeights.Bold;
+            textBox.Text = row + "," + col;
+            textBox.Width = SQUARE_SIZE;
+            textBox.Height = SQUARE_SIZE;
+            textBox.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            textBox.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+
+            Thickness margin = textBox.Margin;
+            margin.Left = row * SQUARE_SIZE;
+            margin.Top = col * SQUARE_SIZE;
+            textBox.Margin = margin;
+
+            Border border = new Border();
+            border.CornerRadius = new CornerRadius( 0 );
+
+            textBox.BorderThickness = new Thickness( 1, 1, 1, 1 );
+            textBox.BorderBrush = Brushes.Black;
+
+            if ( row == 2 || row == 5 ) {
+                textBox.BorderThickness = new Thickness( 1, 1, 5, 1 );
+                textBox.BorderBrush = Brushes.Black;
+            }
+
+            if ( col == 2 || col == 5 ) {
+                textBox.BorderThickness = new Thickness( 1, 1, 1, 5 );
+                textBox.BorderBrush = Brushes.Black;
+            }
+            
+            if ((row == 2 && col == 2) || (row == 2 && col == 5) ||
+                (row == 5 && col == 2) || (row == 5 && col == 5)) {
+                textBox.BorderThickness = new Thickness( 1, 1, 5, 5 );
+                textBox.BorderBrush = Brushes.Black;
+            }
+
+            GameBoard.Children.Add( textBox );
         }
     }
 }
